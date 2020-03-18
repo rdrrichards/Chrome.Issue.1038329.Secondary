b8c889c5d7aba434626045aa29538af28bd607d3
Richard Richards <rdrrichards@hotmail.com>
2020-03-18 07:14:32 -0500

    pre update commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('ng-viewer-app');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng-viewer-app app is running!');
-  });
 });
