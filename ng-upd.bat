94d9965778437e2fbc1c35f0df4df62cc4b4f7ec
Richard Richards <rdrrichards@hotmail.com>
2023-06-22 04:25:11 -0500

    pre update commit

--- ng-upd.bat
+++ ng-upd.bat
@@ -1,8 +1,8 @@
 CALL git commit -am "pre update commit"
-CALL ng update @angular/cli --force
+CALL ng update @angular/core --force
 CALL git add .
 CALL git commit -am "cli update to latest"
-CALL ng update @angular/core --force
+CALL ng update @angular/cli --force
 CALL git add .
 CALL git commit -am "ng update to latest"
 CALL npm audit fix
